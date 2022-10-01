37a0f0ccf501cacf9b7ded881b90f982c3dcde04
Nithish Kumar <nithssh@protonmail.com>
2022-10-01 22:53:43 +0530

    Change analytics

--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -1,10 +1,19 @@
+import { DBClient } from '../database.js';
 import { Router } from 'express';
 
 export const analyticsRouter = Router()
+const dbCon = new DBClient()
+const analy = dbCon.getDb().collection('analytics')
 
 analyticsRouter.route('/views')
     .get(async function (req, res, next) {
-        console.log('recieved visit');
+        analy.insertOne({
+            "metadata": {
+                "type": "view",
+                "url": "/"
+            },
+            "timestamp": new Date().toISOString()
+        })
         res.sendStatus(200);
     })
 
